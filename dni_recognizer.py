81c74973343211a464c499d2f60d2e9a96d1f8ce
caarlota4 <100429099@alumnos.uc3m.es>
2021-02-18 21:46:45 +0100

    push4

--- dni_recognizer.py
+++ dni_recognizer.py
@@ -1,6 +1,8 @@
 def recognizer_nie(ID):
     letter = ID[0]
+    number = ID[1:7]
+    number = int(number)
 
-def recognizer_nif(dni)
+def recognizer_nif(dni):
    letter=dni[-1]
    number=dni[0:7]
