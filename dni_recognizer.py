54d86416c1f301f267bbdf3dc6706f73a8fd15af
lucia-ferrer <100429154@alumnos.uc3m.es>
2021-02-19 23:05:56 +0100

    converse_letter

--- dni_recognizer.py
+++ dni_recognizer.py
@@ -6,3 +6,8 @@ def recognizer_nie(ID):
 def recognizer_nif(dni):
    letter=dni[-1]
    number=int(dni[0:7])
+   if letter == converse_letter(number):
+       return 0
+   return -1
+
+def converse_letter(n):
