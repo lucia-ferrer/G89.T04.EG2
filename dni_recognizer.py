09dc4875d2cef24ee7a8aa436bad50aeab7e64c7
caarlota4 <100429099@alumnos.uc3m.es>
2021-02-21 17:02:21 +0100

    corrected errors

--- dni_recognizer.py
+++ dni_recognizer.py
@@ -1,14 +1,15 @@
 def recognizer_nie(id):
+    first_letter = {x:0,y:1,z:2}
     letter = id[0]
-    number = id[1:7]
-    number = int(number)
+    number = int(id[1:6])
+
 
 def recognizer_nif(dni):
-   letter=dni[-1]
-   number=int(dni[0:7])
-   if letter == converse_letter(number):
-       return 0
-   return -1
+    letter=dni[-1]
+    number=int(dni[0:7])
+    if letter == converse_letter(number):
+        return 0
+    return -1
 
 def converse_letter(n):
     return "L"
