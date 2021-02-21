705c00f662a6e140d8376965d22fe603bb01f5bf
lucia-ferrer <100429154@alumnos.uc3m.es>
2021-02-21 18:07:16 +0100

    update

--- dni_recognizer.py
+++ dni_recognizer.py
@@ -1,15 +1,16 @@
-def recognizer_nie(id):
-    first_letter = {"X":0, "Y":1, "Z":2}
-    letter = id[0]
+def recognizer_nie(nie):
+    first_letter = {"X": 0, "Y": 1, "Z": 2}
+    letter = nie[0]
     last_number = first_letter[letter]
-    real_number = int(id[7])
+    real_number = int(nie[7])
     if last_number != real_number:
         return -1
-    number = int(id[1:7])
+    number = int(nie[1:7])
     real_letter = converse_letter(number)
-    if real_letter != id[8]:
+    if real_letter != nie[8]:
         return -1
     return 0
+
 
 def recognizer_nif(dni):
     letter = dni[-1]
@@ -18,10 +19,10 @@ def recognizer_nif(dni):
         return 0
     return -1
 
-def converse_letter(n):
-    letters = {0:"T", 1:"R", 2:"W", 3: "A", 4:"G", 5:"M", 6:"Y",
-               7:"F", 8:"P", 9:"D", 10:"X", 11:"B", 12:"N", 13:"J",
-               14:"Z", 15:"S", 16:"Q", 17:"V", 18:"H", 19:"L", 20:"C", 21:"K", 22:"E"}
-    number = n%23
-    return letters[number]
 
+def converse_letter(n):
+    letters = {0: "T", 1: "R", 2: "W", 3: "A", 4: "G", 5: "M", 6: "Y",
+               7: "F", 8: "P", 9: "D", 10: "X", 11: "B", 12: "N", 13: "J",
+               14: "Z", 15: "S", 16: "Q", 17: "V", 18: "H", 19: "L", 20: "C", 21: "K", 22: "E"}
+    number = n % 23
+    return letters[number]
