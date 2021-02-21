05b2d2ad0f1163973f145dff9815fa7b19b81bc2
caarlota4 <100429099@alumnos.uc3m.es>
2021-02-21 17:36:06 +0100

    converse_letter

--- dni_recognizer.py
+++ dni_recognizer.py
@@ -1,8 +1,15 @@
 def recognizer_nie(id):
-    first_letter = {x:0,y:1,z:2}
+    first_letter = {"x":0,"y":1,"z":2}
     letter = id[0]
-    number = int(id[1:6])
-
+    last_number = first_letter[letter]
+    real_number = int(id[7])
+    if last_number != real_number:
+        return -1
+    number = int(id[1:7])
+    real_letter = converse_letter(number)
+    if real_letter != id[8]:
+        return -1
+    return 0
 
 def recognizer_nif(dni):
     letter=dni[-1]
@@ -12,4 +19,6 @@ def recognizer_nif(dni):
     return -1
 
 def converse_letter(n):
-    return "L"
+    letters = {0:"T",1:"R",2:"W",3:"A",4:"G",5:"M",6:"Y",7:"F",8:"P",9:"D",10:"X",11:"B",12:"N",13:"J",14:"Z",15:"S",16:"Q",17:"V",18:"H",19:"L",20:"C",21:"K",22:"E"}
+    number = n/23
+    return letters[number]
