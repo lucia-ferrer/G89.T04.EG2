4e47ebc22f3ea66d0167ac06874f8c59062bddbe
lucia-ferrer <100429154@alumnos.uc3m.es>
2021-02-26 14:00:13 +0100

    AccessRequest Update

--- code_v2 (2)/SecureAll/AccessRequest.py
+++ code_v2 (2)/SecureAll/AccessRequest.py
@@ -1,6 +1,8 @@
+""" Import """
 import json
 from datetime import datetime
 
+""" This contains only 1 class"""
 class AccessRequest:
     def __init__(self, id_document, full_name):
         self.__name = full_name
@@ -11,6 +13,7 @@ class AccessRequest:
     def __str__(self):
         return "AccessRequest:" + json.dumps(self.__dict__)
 
+    """ This contains 2 functions """
     @property
     def name(self):
         return self.__name
@@ -18,9 +21,10 @@ class AccessRequest:
     def name(self, value):
         self.__name = value
 
+    """ This function contains 2 functions """
     @property
     def id_document(self):
         return self.__id_document
-    @name.setter
+    @id_document.setter
     def id_document(self, value):
         self.__id_document = value
