7774cddd4d53b8beb51abf80fa074c0bd9b9bfda
lucia-ferrer <100429154@alumnos.uc3m.es>
2021-02-26 14:15:06 +0100

    AccessManagementException Update

--- code_v2 (2)/SecureAll/AccessManagementException.py
+++ code_v2 (2)/SecureAll/AccessManagementException.py
@@ -1,13 +1,18 @@
+""" This class is to manage exceptions """
+
+
 class AccessManagementException(Exception):
+    """ This class is to manage exceptions """
+
     def __init__(self, message):
         self.__message = message
         super().__init__(self.message)
 
     @property
     def message(self):
+        """ This function requires """
         return self.__message
 
     @message.setter
-    def message(self,value):
+    def message(self, value):
         self.__message = value
-
