1f240294051cba6787f0a8b32707b9b60a2a4426
caarlota4 <100429099@alumnos.uc3m.es>
2021-02-27 14:41:11 +0100

    init corrections

--- code_v2 (2)/SecureAll/__init__.py
+++ code_v2 (2)/SecureAll/__init__.py
@@ -1,3 +1,4 @@
-from .AccessRequest import AccessRequest
+"""Here starts the init."""
 from .AccessManager import AccessManager
+from .AccessRequest import AccessRequest
 from .AccessManagementException import AccessManagementException
